b46aab17127dc3c3b10e3e3f3abce6b4cff2e261
YuLong Yao <feilongphone@gmail.com>
2022-08-23 19:50:49 +0800

    platform: tangnano4k: add test platform example

---
+++ src/main/scala/platforms/tangnano4k.scala
@@ -0,0 +1,28 @@
+package gowin.platforms
+
+import chisel3._
+import chisel3.util._
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+
+import fpgamacro.gowin._
+
+class TangNano4k extends RawModule {
+  /* Clock and reset */
+  val clk_xtal = IO(Input(Clock()))
+  val reset_button = IO(Input(Bool()))
+
+  /* Debug leds */
+  val led = IO(Output(Bool()))
+
+  withClockAndReset(clk_xtal, reset_button) {
+    val mcu = Module(new EmcuModule())
+    mcu.io.rtc_clk := clk_xtal
+  }
+
+  led := reset_button
+}
+
+object TangNano4k extends App {
+  (new ChiselStage)
+    .execute(args, Seq(ChiselGeneratorAnnotation(() => new TangNano4k())))
+}
