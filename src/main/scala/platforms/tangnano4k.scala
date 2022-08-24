65fd7f21f7a2998bdc873e872303350a2f20327b
YuLong Yao <feilongphone@gmail.com>
2022-08-24 20:03:02 +0800

    platform: tangnano4k: add sram

--- src/main/scala/platforms/tangnano4k.scala
+++ src/main/scala/platforms/tangnano4k.scala
@@ -11,34 +11,76 @@ class BasicIO extends Bundle {
   val reset_button = Input(Bool())
 
   /* Debug leds */
-  val led = Output(Bool())
 }
 
-class TangNano4k extends RawModule {
-  // Basic IO
-  val io = IO(new BasicIO())
+class BSRAM_SP_Module extends Module {
+  val ram = IO(Flipped(new SRAM))
+
+  val bsram0 = Module(new SP)
+  val bsram1 = Module(new SP)
+  val bsram2 = Module(new SP)
+  val bsram3 = Module(new SP)
+
+  bsram0.io.CLK := clock
+  bsram1.io.CLK := clock
+  bsram2.io.CLK := clock
+  bsram3.io.CLK := clock
+
+  bsram0.io.RESET := reset
+  bsram1.io.RESET := reset
+  bsram2.io.RESET := reset
+  bsram3.io.RESET := reset
+
+  bsram0.io.CE := ram.cs
+  bsram1.io.CE := ram.cs
+  bsram2.io.CE := ram.cs
+  bsram3.io.CE := ram.cs
+
+  bsram0.io.WRE := ram.wren(0)
+  bsram1.io.WRE := ram.wren(1)
+  bsram2.io.WRE := ram.wren(2)
+  bsram3.io.WRE := ram.wren(3)
+
+  bsram0.io.AD := Cat(ram.addr, 0.U(1.W))
+  bsram1.io.AD := Cat(ram.addr, 0.U(1.W))
+  bsram2.io.AD := Cat(ram.addr, 0.U(1.W))
+  bsram3.io.AD := Cat(ram.addr, 0.U(1.W))
+
+  ram.rdata := bsram0.io.DO
+  ram.rdata := bsram1.io.DO
+  ram.rdata := bsram2.io.DO
+  ram.rdata := bsram3.io.DO
+
+  bsram0.io.DI := ram.wdata
+  bsram1.io.DI := ram.wdata
+  bsram2.io.DI := ram.wdata
+  bsram3.io.DI := ram.wdata
+}
+
+class TangNano4k extends Module {
+  // debug led
+  val led = IO(Output(Bool()))
 
   // GPIO
   val gpio = IO(Output(Bool()))
 
   // debug port
   // val dap = IO(new DAP()) // debug port
-  val sram0 = IO(new SRAM)
 
-  withClockAndReset(io.clk_xtal, io.reset_button) {
-    val mcu = Module(new EmcuModule())
-    mcu.io.rtc_clk := io.clk_xtal
-    mcu.io.dap <> DontCare
-    mcu.io.gpio.input <> 0x5555.U(16.W)
-    mcu.io.gpio.output <> DontCare
-    mcu.io.gpio.output_enable <> DontCare
+  // withClockAndReset(io.clk_xtal, io.reset_button) {
+  val mcu = Module(new EmcuModule())
+  val sram0 = Module(new BSRAM_SP_Module)
 
-    gpio := mcu.io.gpio.output(0)
+  mcu.io.rtc_clk := clock
+  mcu.io.dap <> DontCare
+  mcu.io.gpio.input <> 0x5555.U(16.W)
+  mcu.io.gpio.output_enable <> DontCare
 
-    mcu.io.sram0 <> sram0
-  }
+  gpio := mcu.io.gpio.output(0)
 
-  io.led := io.reset_button
+  mcu.io.sram0 <> sram0.ram
+
+  led := reset
 }
 
 object TangNano4k extends App {
