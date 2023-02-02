090df3150f9df80c9003b6478cf718197cb3164b
YuLong Yao <feilongphone@gmail.com>
2023-02-03 06:07:45 +0800

    board: gowin: empu: add basic support
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

---
+++ boards/common/openFPGALoader.board.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2023 YuLong Yao <feilongphone@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+board_set_flasher_ifnset(openFPGALoader)
+board_finalize_runner_args(openFPGALoader)
