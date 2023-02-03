0417116930dda01963c5661bfbc724164041a016
YuLong Yao <feilongphone@gmail.com>
2023-02-04 07:28:32 +0800

    samples: basic: led: change gpio to normal way
    samples: basic: led: change gpio to normal way
    
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -1,22 +1,42 @@
 /*
- * Copyright (c) 2023 YuLong Yao <feilongphone@gmail.com>
+ * Copyright (c) 2016 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
 
-#define GPIO_BASE 0x40010000
-#define GPIO_DATAOUT *(volatile unsigned short *)(GPIO_BASE + 0x0004)
-#define GPIO_OUTENSET *(volatile unsigned short *)(GPIO_BASE + 0x0010)
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
 
-void main() {
-  GPIO_OUTENSET |= 0x01;
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
 
-  while (1) {
-    GPIO_DATAOUT ^= 0x01;
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-    k_msleep(1000);
-  }
+void main(void)
+{
+	int ret;
+
+	if (!gpio_is_ready_dt(&led)) {
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return;
+		}
+		k_msleep(SLEEP_TIME_MS);
+	}
 }
